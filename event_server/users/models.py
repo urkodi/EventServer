92db5898d48575e79a7da97adee752c94827b7a5
urkodi <damilyolivo@gmail.com>
2025-11-26 00:24:24 -0500

    Fixed cookies

--- event_server/users/models.py
+++ event_server/users/models.py
@@ -1,28 +1,35 @@
+from enum import unique
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
-class User(models.Model):
+
+class User(AbstractBaseUser):
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
-    email = models.CharField(max_length=254)
-    password_hash = models.CharField()
-    pfp_path = models.CharField(default= None, blank= True, null= True)
+    email = models.EmailField(unique=True)
+    password = models.CharField()
+    pfp_path = models.CharField(default=None, blank=True, null=True)
 
-    def __str__ (self):
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name", "password"]
+
+    def __str__(self):
         return f"{self.first_name} {self.last_name}"
-    
+
+
 class UserInteractions(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    event_id = models.ForeignKey('events.Event', on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(default= None, blank= True, null= True)
+    event_id = models.ForeignKey("events.Event", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=None, blank=True, null=True)
+
 
 class UserBookmarks(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    event_id = models.ForeignKey('events.Event', on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(default= None, blank= True, null= True)
+    event_id = models.ForeignKey("events.Event", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=None, blank=True, null=True)
+
 
 class UserTickets(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    event_id = models.ForeignKey('events.Event', on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(default= None, blank= True, null= True)
-
-    
+    event_id = models.ForeignKey("events.Event", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(default=None, blank=True, null=True)
